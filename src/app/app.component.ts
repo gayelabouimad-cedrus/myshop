18acd5061b2bb22723c841a466b954f4461133a3
Gayel Abou Imad <gayelabouimad-cedrus>
2020-09-24 16:18:28 +0300

    app home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isChecked = false;
+  value = '';
   title = 'myshop';
+
+  menuItems: MenuItem[] = [
+    {
+      label: 'Men',
+      icon: 'sports_soccer',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Women',
+      icon: 'sports_soccer',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Kids',
+      icon: 'sports_soccer',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Electronics',
+      icon: 'notes',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Fitness Machines',
+      icon: 'notes',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Online Exclusive',
+      icon: 'rss_feed',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'About Us',
+      icon: 'help',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+
+  ];
+
+  tooog(value) {
+    this.isChecked = !this.isChecked;
+    if (value.checked) {
+      document.body.classList.remove('my-light-theme');
+      document.body.classList.add('my-dark-theme');
+    } else {
+      document.body.classList.remove('my-dark-theme');
+      document.body.classList.add('my-light-theme');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { FormsModule } from '@angular/forms'; 
+
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -14,7 +26,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     MatToolbarModule,
     MatIconModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
